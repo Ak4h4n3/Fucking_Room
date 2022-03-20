1f9cedb60be73656037d712404e60854cd9b7ff0
akahane64 <akahane64@gmail.com>
2022-03-20 21:47:15 +0900

    test

---
+++ Assets/Scenes/SampleScene_UdonProgramSources/Cube.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Cube : UdonSharpBehaviour
+{
+    public GameObject toggleObject;
+    public AudioSource audioSource;
+
+    public override void Interact(){
+        audioSource.Play();
+    }
+}
